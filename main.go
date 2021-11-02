2593eaf91107615ee9226ec7a6de9b381fd4ba5e
Alex Saunders <acsauk@gmail.com>
2021-11-02 16:34:43 +0000

    Get list of banks

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -19,6 +20,7 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	}
 
+	// Get access token
 	body, err := json.Marshal(map[string]string{
 		"secret_id": os.Getenv("SECRET_ID"),
 		"secret_key": os.Getenv("SECRET_KEY"),
@@ -42,5 +44,47 @@ func main() {
 		log.Fatal(err)
 	}
 
+	type tokenResponse struct {
+		AccessToken string `json:"access"`
+	}
+
+	t := tokenResponse{}
+	json.Unmarshal(body, &t)
+
+	token := fmt.Sprintf("Bearer %s", t.AccessToken)
+
+	// Get available banks
+	client := http.Client{}
+	req , err := http.NewRequest("GET", "https://ob.nordigen.com/api/v2/institutions/?country=gb", nil)
+	if err != nil {
+		//Handle Error
+	}
+
+	log.Println(token)
+
+	req.Header = http.Header{
+		"Host": []string{"www.host.com"},
+		"Content-Type": []string{"application/json"},
+		"Authorization": []string{token},
+	}
+
+	resp, err = client.Do(req)
+	if err != nil {
+		//Handle Error
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Println(string(body))
+
 }
