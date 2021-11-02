3faf64809a34c0a3da2089faac476a2930d9e4bf
Alex Saunders <acsauk@gmail.com>
2021-11-02 16:13:56 +0000

    Get nordigen access token

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/acsauk/go-open-banking
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.6.1
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/labstack/echo/v4 v4.6.1
+)
--- main.go
+++ main.go
@@ -1,32 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load .env file
+	err := godotenv.Load()
 
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
-
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		httpPort = "8080"
+	if err != nil {
+		log.Fatalf("Error loading .env file")
 	}
 
-	e.Logger.Fatal(e.Start(":" + httpPort))
+	body, err := json.Marshal(map[string]string{
+		"secret_id": os.Getenv("SECRET_ID"),
+		"secret_key": os.Getenv("SECRET_KEY"),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := http.Post("https://ob.nordigen.com/api/v2/token/new/", "application/json", bytes.NewBuffer(body))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(body))
 }
