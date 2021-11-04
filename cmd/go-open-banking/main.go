eb5254a32fca99f243375822cca2016fded6946d
Alex Saunders <acsauk@gmail.com>
2021-11-04 17:07:33 +0000

    Extract available banks to nordigenAPI

--- cmd/go-open-banking/main.go
+++ cmd/go-open-banking/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"net/http"
-
 	"github.com/acsauk/go-open-banking/internal/nordigenAPI"
 	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
@@ -18,38 +15,7 @@ func main() {
 	}
 
 	accessBearerToken := nordigenAPI.GetBearerAccessToken()
-
-	// Get available banks
-	client := http.Client{}
-	req, err := http.NewRequest("GET", "https://ob.nordigen.com/api/v2/institutions/?country=gb", nil)
-	if err != nil {
-		//Handle Error
-	}
-
 	log.Println(accessBearerToken)
 
-	req.Header = http.Header{
-		"Host":          []string{"www.host.com"},
-		"Content-Type":  []string{"application/json"},
-		"Authorization": []string{accessBearerToken},
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		//Handle Error
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println(string(body))
+	nordigenAPI.GetAvailableBanks(accessBearerToken)
 }
--- internal/nordigenAPI/nordigen-api.go
+++ internal/nordigenAPI/nordigen-api.go
@@ -44,3 +44,37 @@ func GetBearerAccessToken() string {
 
 	return fmt.Sprintf("Bearer %s", t.AccessToken)
 }
+
+func GetAvailableBanks(accessBearerToken string){
+	// Get available banks
+	client := http.Client{}
+	req, err := http.NewRequest("GET", "https://ob.nordigen.com/api/v2/institutions/?country=gb", nil)
+	if err != nil {
+		//Handle Error
+	}
+
+	req.Header = http.Header{
+		"Host":          []string{"www.host.com"},
+		"Content-Type":  []string{"application/json"},
+		"Authorization": []string{accessBearerToken},
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		//Handle Error
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(string(body))
+}
